2fbeb5752c171226610590986f3528896a819bb1
Kenny Chong <kennychong89@gmail.com>
2016-05-13 12:48:15 -0400

    Performed minor refactoring on tictactoe_bonus_features.rb

--- exercises/lesson_4/tictactoe_bonus_features.rb
+++ exercises/lesson_4/tictactoe_bonus_features.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
 # Tic Tac Toe with bonus features
 # Kenny Chong
 # 05/04/2016
-require 'pry'
+require 'yaml'
+MESSAGES = YAML.load_file('tic_tac_toe_bonus_features.yml')
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -19,32 +21,7 @@ def prompt(msg)
 end
 
 def clear_screen
-  system('clear')
-end
-
-# rubocop:disable Metrics/MethodLength
-def display_board(brd)
-  puts "You are a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
-  puts ""
-  puts "     |     |"
-  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
-  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
-  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
-  puts "     |     |"
-  puts ""
-end
-# rubocop:enable Metrics/MethodLength Abc/Size
-
-def initialize_board
-  new_board = {}
-  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
-  new_board
+  system('clear') || system('cls')
 end
 
 def empty_squares(brd)
@@ -53,7 +30,6 @@ end
 
 def joiner(squares, seperator, end_seperator)
   joined_string = ''
- 
   if squares.count == 1
     joined_string = squares[0]
   elsif squares.count == 2
@@ -74,43 +50,20 @@ def player_places_piece!(brd)
     square = gets.chomp.to_i
 
     break if empty_squares(brd).include?(square)
-    prompt "Sorry, that's is not a valid choice."
+    prompt MESSAGES['invalid_square_to_pick']
   end
 
-  brd[square] = PLAYER_MARKER
+  mark_square_at_location!(brd, square, PLAYER_MARKER)
+end
 
-  square
+def mark_square_at_location!(brd, square_location, marker)
+  brd[square_location] = marker
 end
 
 ### COMPUTER OPPONENT RELATED METHODS ###
-def semi_smart_computer_places_piece!(brd)
-  winning_square = find_winning_square_location(brd)
-  danger_square = find_danger_square_location(brd)
-
-  if winning_square
-    brd[winning_square] = COMPUTER_MARKER
-  elsif danger_square
-    brd[danger_square] = COMPUTER_MARKER
-  elsif square_is_empty(brd, 5)
-    brd[5] = COMPUTER_MARKER
-  else
-    computer_places_piece_at_random!(brd)
-  end 
-end
-
-def computer_places_piece_at_random!(brd)
-  square = empty_squares(brd).sample
-  brd[square] = COMPUTER_MARKER
-end
-
-def offensive_computer_places_piece!(brd)
-  winning_square = find_winning_square_location(brd)
-
-  if winning_square
-    brd[winning_square] = COMPUTER_MARKER
-  else
-    computer_places_piece_at_random!(brd)
-  end
+def computer_place_random_piece!(brd)
+  random_square_location = empty_squares(brd).sample
+  mark_square_at_location!(brd, random_square_location, COMPUTER_MARKER)
 end
 
 def defensive_computer_places_piece!(brd)
@@ -118,81 +71,87 @@ def defensive_computer_places_piece!(brd)
   danger_square = find_danger_square_location(brd)
 
   if danger_square
-    brd[danger_square] = COMPUTER_MARKER
+    mark_square_at_location!(brd, danger_square, COMPUTER_MARKER)
   else
-    computer_places_piece_at_random!(brd)
-  end 
-end
-
-def find_winning_square_location(brd)
-  WINNING_LINES.each do |line|
-    if brd.values_at(*line).count(COMPUTER_MARKER) == 2 && line_has_empty_space(brd, line)
-      return line_get_empty_space(brd, line)
-    end
-  end
-  nil
-end
-
-def find_danger_square_location(brd)
-  danger_line = nil
-  #selected_lines = filter_lines(player_marked_square_index)
-  
-  WINNING_LINES.each do |line|
-    danger_line = line if is_a_danger_line(brd, line)
-    break unless danger_line == nil  
-  end
-    
-  line_get_empty_space(brd, danger_line) if danger_line != nil
-end
-
-def filter_lines(selected_square_index)
-  lines = WINNING_LINES.select do |line|
-    line.include?(selected_square_index)
+    computer_place_random_piece!(brd)
   end
 end
 
-def is_a_danger_line(brd, line)
-  count = 0
-  
-  line.each do |square_index|
-    count = count + 1 if brd[square_index] == PLAYER_MARKER 
-  end
+def offensive_computer_places_piece!(brd)
+  winning_square = find_winning_square_location(brd)
 
-  count == 2 && line_has_empty_space(brd, line)
+  if winning_square
+    mark_square_at_location!(brd, winning_square, COMPUTER_MARKER)
+  else
+    computer_place_random_piece!(brd)
+  end
 end
 
-def line_has_empty_space(brd, line)
-  line.each do |square_index|
-    return true if brd[square_index] == INITIAL_MARKER   
-  end
+def semi_smart_computer_places_piece!(brd)
+  winning_square = find_winning_square_location(brd)
+  danger_square = find_danger_square_location(brd)
 
-  false
-end
-
-def line_get_empty_space(brd, line)
-  line.each do |square_index|
-    return square_index if brd[square_index] == INITIAL_MARKER   
+  if winning_square
+    mark_square_at_location!(brd, winning_square, COMPUTER_MARKER)
+  elsif danger_square
+    mark_square_at_location!(brd, danger_square, COMPUTER_MARKER)
+  elsif square_is_empty(brd, 5)
+    mark_square_at_location!(brd, 5, COMPUTER_MARKER)
+  else
+    computer_place_random_piece!(brd)
   end
 end
 
 def square_is_empty(brd, square_index)
   brd[square_index] == INITIAL_MARKER
 end
-#########################################
 
-def board_full?(brd)
-  empty_squares(brd).empty?
+def find_winning_square_location(brd)
+  WINNING_LINES.each do |line|
+    if a_winning_line?(brd, line) && line_has_empty_square?(brd, line)
+      return line_get_empty_square(brd, line)
+    end
+  end
+  nil
 end
 
-def someone_won?(brd)
-  !!detect_winner(brd)
+def find_danger_square_location(brd)
+  WINNING_LINES.each do |line|
+    if a_danger_line?(brd, line) && line_has_empty_square?(brd, line)
+      return line_get_empty_square(brd, line)
+    end
+  end
+  nil
+end
+
+def a_winning_line?(brd, line)
+  count_markers_in_a_line(brd, line, COMPUTER_MARKER) == 2
+end
+
+def a_danger_line?(brd, line)
+  count_markers_in_a_line(brd, line, USER_MARKER) == 2
+end
+
+def line_has_empty_square?(brd, line)
+  count_markers_in_a_line(brd, line, INITIAL_MARKER) == 1
+end
+
+def line_get_empty_square(brd, line)
+  line.each do |square_index|
+    return square_index if brd[square_index] == INITIAL_MARKER
+  end
+end
+#########################################
+
+def count_markers_in_a_line(brd, line, marker)
+  brd.values_at(*line).count(marker)
 end
 
 def detect_winner(brd)
   WINNING_LINES.each do |line|
-    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+    if count_markers_in_a_line(brd, line, PLAYER_MARKER) == 3
       return PLAYER_NAME
-    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+    elsif count_markers_in_a_line(brd, line, COMPUTER_MARKER) == 3
       return COMPUTER_NAME
     end
   end
@@ -200,7 +159,7 @@ def detect_winner(brd)
 end
 
 def update_score(score_card, winner_name)
-  score_card[winner_name] += 1 if winner_name != nil
+  score_card[winner_name] += 1 if winner_name
 end
 
 def display_score(score_card)
@@ -211,19 +170,15 @@ def display_score(score_card)
 end
 
 def display_game_winner(score_card)
-  score_card.each do |name, score| 
+  score_card.each do |name, score|
     if score == MATCH_SCORE
       prompt "The winner is #{name}! Congrats!"
     end
   end
 end
 
-def initialize_score_card
-  { PLAYER_NAME => 0, COMPUTER_NAME => 0 }
-end
-
 def play_again?
-  prompt "Play again? (y or n)"
+  prompt MESSAGES['play_again']
   answer = gets.chomp
 
   answer
@@ -233,12 +188,68 @@ def display_current_round_results(board)
   if someone_won?(board)
     prompt "#{detect_winner(board)} won the round!"
   else
-    prompt "It's a tie!"
+    prompt MESSAGES['tie']
   end
 end
 
 def reached_max_points?(player_name, score_card, winning_match_point)
   score_card[player_name] == winning_match_point
+end
+
+def place_piece!(brd, current_player)
+  if current_player == COMPUTER_NAME
+    semi_smart_computer_places_piece!(brd)
+  else
+    player_places_piece!(brd)
+  end
+end
+
+def ask_player_to_choose
+  choice = ''
+  loop do
+    prompt MESSAGES['who_goes_first']
+    choice = gets.chomp
+
+    break unless choice != "1" && choice != "2"
+
+    prompt MESSAGES['invalid_choice_to_go_first']
+  end
+
+  if choice == "2"
+    COMPUTER_NAME
+  else
+    PLAYER_NAME
+  end
+end
+
+def create_display_board
+  MESSAGES['display_board'].clone
+end
+
+def alternate(current_player)
+  if current_player == COMPUTER_NAME
+    PLAYER_NAME
+  else
+    COMPUTER_NAME
+  end
+end
+
+def display_board(brd)
+  puts "You are a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
+  display_board = create_display_board
+  brd.each do |num, marker|
+    display_board.sub!("(" + num.to_s + ")", " " + marker + " ") unless marker == INITIAL_MARKER
+    # Note: Is there a better way to append () to num and " " to marker?
+  end
+  puts display_board
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
 end
 
 def play_round(board)
@@ -254,38 +265,14 @@ def play_round(board)
   end
 end
 
-def place_piece!(brd, current_player)
-  if current_player == COMPUTER_NAME
-    semi_smart_computer_places_piece!(brd)
-  else
-    player_places_piece!(brd)
-  end
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  new_board
 end
 
-def alternate(current_player)
-  if current_player == COMPUTER_NAME
-    PLAYER_NAME
-  else
-    COMPUTER_NAME
-  end
-end
-
-def ask_player_to_choose
-  choice = ''
-  loop do
-    prompt "Type 1 for player to go first. Type 2 for computer to go first."
-    choice = gets.chomp
-
-    break unless choice != "1" && choice != "2"
-
-    prompt "Invalid choice. Please press 1 for player to go or 2 for computer to go."
-  end
-
-  if choice == "2" 
-    COMPUTER_NAME 
-  else 
-    PLAYER_NAME
-  end
+def initialize_score_card
+  { PLAYER_NAME => 0, COMPUTER_NAME => 0 }
 end
 
 def run_game_loop
@@ -298,12 +285,10 @@ def run_game_loop
     update_score(score_card, winner_name)
     display_board(board)
     display_current_round_results(board)
-    display_score(score_card)  
-    
-    break if reached_max_points?(winner_name, score_card, winning_match_point) ||
-             reached_max_points?(winner_name, score_card, winning_match_point)
+    display_score(score_card)
+    break if reached_max_points?(winner_name, score_card, winning_match_point)
 
-    prompt "Press enter to start next round."
+    prompt MESSAGES['start_next_round']
     gets.chomp
   end
 end
@@ -316,10 +301,7 @@ def start_game
     break unless continue.downcase.start_with?('y')
   end
 
-  prompt "Thanks for playing Tic Tac Toe! Later!"
+  prompt MESSAGES['thanks_for_playing']
 end
 
 start_game
-
-
-
