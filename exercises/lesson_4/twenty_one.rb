4d0b4b8cdb1543bfee56b1a7dece2f6048f32908
Kenny Chong <kennychong89@gmail.com>
2016-05-28 15:49:56 -0400

    Refactored twenty_one.rb following Rubocop's suggestions.

--- exercises/lesson_4/twenty_one.rb
+++ exercises/lesson_4/twenty_one.rb
@@ -2,8 +2,8 @@
 # Twenty-One
 # Kenny Chong
 require 'pry'
-SUITS = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-VALUES = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+SUITS = ['Clubs', 'Diamonds', 'Hearts', 'Spades'].freeze
+VALUES = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'].freeze
 MAX_SCORE_BEFORE_BUST = 21
 MAX_SCORE_BEFORE_DEALER_HOLDS = 17
 PLAYER_NAME = "Player"
@@ -23,7 +23,7 @@ def start_game
     run_game_loop
     prompt "Do you want to play again? ('y' or 'n')."
     decision = gets.chomp
-    break unless decision.downcase.start_with?('y')
+    break unless decision.downcase.start_with?('y') 
   end
   prompt "Thank you for playing! Goodbye!"
 end
@@ -33,13 +33,13 @@ def prompt(msg)
 end
 
 def run_game_loop
-  rounds_won_card = {PLAYER_NAME => 0, DEALER_NAME => 0}
+  rounds_won_card = { PLAYER_NAME => 0, DEALER_NAME => 0 }
   rounds = 1
   loop do
     prompt "ROUND #{rounds}"
     winner_name = play_a_round_of_21
     if there_is_winner(winner_name)
-      give_winner_a_round_point(rounds_won_card, winner_name) 
+      give_winner_a_round_point(rounds_won_card, winner_name)
       break if game_over(rounds_won_card, winner_name)
     end
     rounds += 1
@@ -48,24 +48,22 @@ def run_game_loop
     gets.chomp
     clear_screen
   end
+  display_round_scores(rounds_won_card)
 end
 
 def play_a_round_of_21
   deck = create_deck_and_shuffle
   player_hand = create_hand(2, deck)
   dealer_hand = create_hand(2, deck)
-  score_tracker = {PLAYER_NAME => 0, DEALER_NAME => 0}
+  score_tracker = { PLAYER_NAME => 0, DEALER_NAME => 0 }
   update_score(score_tracker, PLAYER_NAME, calculate_hand(player_hand))
   update_score(score_tracker, DEALER_NAME, calculate_hand(dealer_hand))
   display_person_hand_and_score(player_hand, PLAYER_NAME, score_tracker)
   display_person_hand_and_score(dealer_hand, DEALER_NAME, score_tracker)
-  
   player_turn(player_hand, dealer_hand, score_tracker, deck)
-  if !dealer_wins?(score_tracker) && !dealer_reached_hit_limit?(get_score(score_tracker, DEALER_NAME))
-    dealer_turn(dealer_hand, player_hand, score_tracker, deck)
-  end
+  dealer_turn(dealer_hand, player_hand, score_tracker, deck)
   display_round_results(score_tracker)
-  get_winner_name(score_tracker) unless is_tied(score_tracker)
+  get_winner_name(score_tracker) unless tied?(score_tracker)
 end
 
 def create_deck_and_shuffle
@@ -90,28 +88,27 @@ def draw_card!(deck)
 end
 
 def calculate_hand(hand)
-  score =  0
+  score = 0
   hand.each do |card|
     card_ranking = get_card_ranking(card)
     score += card_ranking
   end
 
-  if is_busted?(score) && how_many_aces_in_hand?(hand) > 0
+  if busted?(score) && how_many_aces_in_hand?(hand) > 0
     score = adjust_score_for_aces(hand, score)
   end
   score
 end
 
 def get_card_ranking(card)
-  card_ranking = 0
   card_value = get_card_value(card)
-  if card_is_an_ace_value?(card_value)
-    card_ranking = ACE_RANKING_HIGH
-  elsif card_is_a_face_value?(card_value)
-    card_ranking = FACE_RANKING
-  else
-    card_ranking = convert_card_value_to_its_numerical_ranking(card_value)
-  end
+  card_ranking = if card_is_an_ace_value?(card_value)
+                   ACE_RANKING_HIGH
+                 elsif card_is_a_face_value?(card_value)
+                   FACE_RANKING
+                 else
+                   convert_card_value_to_its_numerical_ranking(card_value)
+                 end
   card_ranking
 end
 
@@ -125,29 +122,29 @@ end
 
 def card_is_a_face_value?(card_value)
   card_value == 'Jack' ||
-  card_value == 'Queen'||
-  card_value == 'King'
+    card_value == 'Queen' ||
+    card_value == 'King'
 end
 
 def convert_card_value_to_its_numerical_ranking(card_value)
   card_value.to_i
 end
 
-def is_busted?(score)
+def busted?(score)
   score > MAX_SCORE_BEFORE_BUST
 end
 
 def how_many_aces_in_hand?(hand)
-  hand.each.select do |card| 
+  hand.each.select do |card|
     card_is_an_ace_value?(get_card_value(card))
   end.count
 end
 
 def adjust_score_for_aces(hand, score)
   updated_score = score
-  points_to_deduct = ACE_RANKING_HIGH - ACE_RANKING_LOW 
+  points_to_deduct = ACE_RANKING_HIGH - ACE_RANKING_LOW
   how_many_aces_in_hand?(hand).times do
-    updated_score -= points_to_deduct unless !is_busted?(updated_score)
+    updated_score -= points_to_deduct unless !busted?(updated_score)
   end
   updated_score
 end
@@ -165,7 +162,7 @@ def player_turn(player_hand, dealer_hand, score_tracker, deck)
     display_person_hand_and_score(player_hand, PLAYER_NAME, score_tracker)
     display_person_hand_and_score(dealer_hand, DEALER_NAME, score_tracker)
     prompt "\n"
-    break unless !is_busted?(get_score(score_tracker, PLAYER_NAME))
+    break unless !busted?(get_score(score_tracker, PLAYER_NAME))
   end
 end
 
@@ -202,14 +199,14 @@ def display_person_hand_and_score(hand, name, score_tracker)
 end
 
 def show_cards_in_hand(hand)
-  hand.map do |card| 
-      card_value = get_card_value(card)
-      card_ranking = get_card_ranking(card)
-      if card_is_an_ace_value?(card_value) || card_is_a_face_value?(card_value)
-        card_value + "(#{card_ranking})"
-      else
-        get_card_value(card)
-      end
+  hand.map do |card|
+    card_value = get_card_value(card)
+    card_ranking = get_card_ranking(card)
+    if card_is_an_ace_value?(card_value) || card_is_a_face_value?(card_value)
+      card_value + "(#{card_ranking})"
+    else
+      get_card_value(card)
+    end
   end.join(', ')
 end
 
@@ -219,26 +216,26 @@ end
 
 def dealer_wins?(score_tracker)
   player_score = get_score(score_tracker, PLAYER_NAME)
-  dealer_score = get_score(score_tracker, DEALER_NAME) 
-  is_busted?(player_score) || dealer_score > player_score
+  dealer_score = get_score(score_tracker, DEALER_NAME)
+  busted?(player_score) || dealer_score > player_score
 end
 
 def dealer_turn(dealer_hand, player_hand, score_tracker, deck)
-  player_score = get_score(score_tracker, PLAYER_NAME)
   dealer_score = get_score(score_tracker, DEALER_NAME)
-  prompt "#{DEALER_NAME} TURN" 
+  return unless !dealer_wins?(score_tracker) && !dealer_reached_hit_limit?(dealer_score)
+  prompt "#{DEALER_NAME} TURN"
   loop do
     deal(dealer_hand, DEALER_NAME, score_tracker, deck)
     display_person_hand_and_score(player_hand, PLAYER_NAME, score_tracker)
     display_person_hand_and_score(dealer_hand, DEALER_NAME, score_tracker)
     prompt "\n"
-    break unless !dealer_stops?(score_tracker) && !is_busted?(get_score(score_tracker, DEALER_NAME))                   
+    break unless !dealer_stops?(score_tracker) && !busted?(dealer_score)
   end
 end
 
 def dealer_stops?(score_tracker)
   player_score = get_score(score_tracker, PLAYER_NAME)
-  dealer_score = get_score(score_tracker, DEALER_NAME) 
+  dealer_score = get_score(score_tracker, DEALER_NAME)
   dealer_score >= player_score || dealer_reached_hit_limit?(dealer_score)
 end
 
@@ -252,9 +249,9 @@ def display_round_results(score_tracker)
   prompt "Final score is - #{PLAYER_NAME}: #{player_score}, #{DEALER_NAME}: #{dealer_score}"
   if player_score == dealer_score
     prompt "It's a tie!"
-  elsif is_busted?(dealer_score)
+  elsif busted?(dealer_score)
     prompt "#{PLAYER_NAME} have won! #{DEALER_NAME} has busted."
-  elsif is_busted?(player_score)
+  elsif busted?(player_score)
     prompt "#{DEALER_NAME} has won! #{PLAYER_NAME} has busted"
   elsif dealer_score > player_score
     prompt "#{DEALER_NAME} has won!"
@@ -262,9 +259,9 @@ def display_round_results(score_tracker)
     prompt "#{PLAYER_NAME} have won!"
   end
   prompt "\n"
-end 
+end
 
-def is_tied(score_tracker)
+def tied?(score_tracker)
   score_tracker[PLAYER_NAME] == score_tracker[DEALER_NAME]
 end
 
