f8330e6dca7e509cde2ac3357c6293273763cca4
Kenny Chong <kennychong89@gmail.com>
2016-06-04 17:58:16 -0400

    Refactoed twenty_one.rb with Rubocop suggestions. Some name changes.

--- exercises/lesson_4/twenty_one.rb
+++ exercises/lesson_4/twenty_one.rb
@@ -3,8 +3,8 @@
 # Kenny Chong
 # 05/30/2016
 require 'pry'
-SUITS = ['H', 'D', 'S', 'C']
-VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+SUITS = ['H', 'D', 'S', 'C'].freeze
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'].freeze
 MAX_SCORE_BEFORE_BUST = 21
 MAX_SCORE_BEFORE_DEALER_HOLDS = 17
 WINNING_ROUND_POINT = 5
@@ -21,17 +21,17 @@ def initialize_deck
   SUITS.product(VALUES).shuffle
 end
 
-def calculate_total(cards, total)
+def calculate_total(cards)
   values = cards.map { |card| card[1] }
   sum = 0
   values.each do |value|
-    if value == "A"
-      sum += 11
-    elsif value.to_i == 0 # J, Q, K
-      sum += 10
-    else
-      sum += value.to_i
-    end
+    sum += if value == 'A'
+             11
+           elsif value.to_i == 0
+             10
+           else
+             value.to_i
+           end
   end
   correct_for_aces(cards, sum)
 end
@@ -48,48 +48,50 @@ def busted?(total)
 end
 
 # :tie, :dealer, :player, :dealer_busted, :player_busted
-def detect_result(dealer_total, guest_total)
-  if busted?(guest_total)
-    :guest_busted
+def detect_result(dealer_total, player_total)
+  if busted?(player_total)
+    :player_busted
   elsif busted?(dealer_total)
     :dealer_busted
-  elsif dealer_total < guest_total
-    :guest
-  elsif dealer_total > guest_total
+  elsif dealer_total < player_total
+    :player
+  elsif dealer_total > player_total
     :dealer
   else
     :tie
   end
 end
 
-def display_result(dealer_cards, dealer_total, guest_cards, guest_total)
-  result = detect_result(dealer_total, guest_total)
-
+def displayer_winner(result)
   case result
-  when :guest_busted
-    prompt "Guest busted! Dealer wins!"
+  when :player_busted
+    prompt "Player busted! Dealer wins!"
   when :dealer_busted
     prompt "Dealer busted! You win!"
-  when :guest
-    prompt "Guest win!"
+  when :player
+    prompt "Player win!"
   when :dealer
     prompt "Dealer wins!"
   when :tie
     prompt "It's a tie!"
   end
+end
+
+def display_result(dealer_cards, dealer_total, player_cards, player_total)
+  result = detect_result(dealer_total, player_total)
+  displayer_winner(result)
   puts "=============="
   prompt "Dealer has #{dealer_cards}, for a total of: #{dealer_total}"
-  prompt "Guest has #{guest_cards}, for a total of: #{guest_total}"
+  prompt "Player has #{player_cards}, for a total of: #{player_total}"
   puts "=============="
 end
 
-def get_winner_name(guest_total, dealer_total)
-  result = detect_result(dealer_total, guest_total)
-  case result
-  when :guest_busted || :dealer
+def get_winner_name(player_total, dealer_total)
+  result = detect_result(dealer_total, player_total)
+  if result == :player_busted || result == :dealer
     return "Dealer"
-  when :dealer_busted || :guest
-    return "Guest"
+  elsif result == :dealer_busted || result == :player
+    return "Player"
   end
 end
 
@@ -112,38 +114,36 @@ def start_game
 end
 
 def run_game_loop
-  guest_name = "Guest"
-  dealer_name = "Dealer"
-  guest_rounds_won = 0
+  player_rounds_won = 0
   dealer_rounds_won = 0
   loop do
-    winner = play_round(guest_name, dealer_name)
-    if winner == guest_name then guest_rounds_won += 1 end
-    if winner == dealer_name then dealer_rounds_won += 1 end
-    prompt "Rounds Won By #{guest_name}: #{guest_rounds_won}"
-    prompt "Rounds Won By #{dealer_name}: #{dealer_rounds_won}"
-    break if won_game?(guest_rounds_won) || won_game?(dealer_rounds_won)
+    winner = play_round
+    if winner == "Player" then player_rounds_won += 1 end
+    if winner == "Dealer" then dealer_rounds_won += 1 end
+    prompt "Rounds Won By Player: #{player_rounds_won}"
+    prompt "Rounds Won By Dealer: #{dealer_rounds_won}"
+    break if won_game?(player_rounds_won) || won_game?(dealer_rounds_won)
     prompt "Press Enter play next round."
-    gets.chomp 
+    gets.chomp
     clear_screen
   end
 end
 
-def play_round(guest_name, dealer_name)
+def play_round
   deck = initialize_deck
-  guest_cards = []
+  player_cards = []
   dealer_cards = []
-  guest_total = pre_round_setup(guest_cards, deck)
+  player_total = pre_round_setup(player_cards, deck)
   dealer_total = pre_round_setup(dealer_cards, deck)
   prompt "Dealer has #{dealer_cards[0]} and ?"
-  prompt "You have: #{guest_cards[0]} and #{guest_cards[1]}, for a total of #{guest_total}."
+  prompt "Player has: #{player_cards[0]} and #{player_cards[1]}, for a total of #{player_total}."
 
-  guest_total = guest_turn(guest_cards, guest_total, deck)
-  if !busted?(guest_total)
+  player_total = player_turn(player_cards, player_total, deck)
+  if !busted?(player_total)
     dealer_total = dealer_turn(dealer_cards, dealer_total, deck)
   end
-  display_result(dealer_cards, dealer_total, guest_cards, guest_total)
-  get_winner_name(guest_total, dealer_total)
+  display_result(dealer_cards, dealer_total, player_cards, player_total)
+  get_winner_name(player_total, dealer_total)
 end
 
 def pre_round_setup(cards, deck)
@@ -151,42 +151,37 @@ def pre_round_setup(cards, deck)
   2.times do
     card = deck.pop
     cards << card
-    total = calculate_total(cards, total)
+    total = calculate_total(cards)
   end
   total
 end
 
-def guest_turn(guest_cards, guest_total, deck)
+def player_turn(player_cards, player_total, deck)
   loop do
     choice = hit_or_stay?
     if choice == 'h'
       card = deck.pop
-      guest_cards << card
-      guest_total = calculate_total(guest_cards, guest_total)
-      prompt "You chose to hit!"
-      prompt "Your cards are now: #{guest_cards}"
-      prompt "Your total is now: #{guest_total}"
+      player_cards << card
+      player_total = calculate_total(player_cards)
+      prompt "Player chose to hit!"
+      prompt "Player cards are now: #{player_cards}"
+      prompt "Player total is now: #{player_total}"
     end
-    break if choice == 's' || busted?(guest_total) 
+    break if choice == 's' || busted?(player_total)
   end
-  if !busted?(guest_total)
-    prompt "Guest stayed at #{guest_total}"
-  end
-  guest_total
+  if !busted?(player_total) then prompt("Player stayed at #{player_total}") end
+  player_total
 end
 
 def dealer_turn(dealer_cards, dealer_total, deck)
   loop do
     break if busted?(dealer_total) || dealer_total >= 17
     prompt "Dealer hits!"
-    card = deck.pop
     dealer_cards << deck.pop
-    dealer_total = calculate_total(dealer_cards, dealer_total)
+    dealer_total = calculate_total(dealer_cards)
     prompt "Dealer's cards are now: #{dealer_cards}"
   end
-  if !busted?(dealer_total)
-    prompt "Dealer stayed at #{dealer_total}"
-  end
+  if !busted?(dealer_total) then prompt("Dealer stayed at #{dealer_total}") end
   dealer_total
 end
 
